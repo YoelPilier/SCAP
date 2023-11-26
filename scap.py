9e4806d8c2174c52dac90913349f79b779eae59c
Yoel <dexskore@gmail.com>
2023-11-25 22:23:01 -0400

    Add testmusic to playlist

--- scap.py
+++ scap.py
@@ -1,34 +1,5 @@
 ﻿"""
-
-
-player = MusicPlayer()
-player.load_file('test.m4a')
-player.play()  # Reproducir la música
-player.set_volume(0.1)  # Establecer volumen a 50%
-player.get_song_info()  # Obtener información de la canción
-input('Presiona enter para detener la música')
-player.stop()  # Detener la música
-
-input('Presiona enter para reproducir la música')
-
-player.play()  # Reproducir la música
  
-input('Presiona enter para pausar la música')
-
-player.load_file('test.flac')
-
-player.play()  # Reproducir la música
-
-player.get_song_info()  # Obtener información de la canción    
-input('Presiona enter para detener la música') 
-player.load_file('test.mp3')
-print(player.track_playback_time())
-input('Presiona enter para reproducir la música')
-player.play()  # Reproducir la música
-player.get_song_info()  # Obtener información de la canción
-input('Presiona enter para detener la música')
-player.track_playback_time()  # Obtener el tiempo de reproducción
-
 """
 from Player import MusicPlayer
 import PlayList as pl
@@ -39,7 +10,7 @@ player = MusicPlayer()
 playlist = pl.PlayList(player.valid_ext)
 
 
-playlist.add("/home/yoel/externo/Musica/")
+playlist.add("testmusic")
 
 playlist.shuffle = True
 
